272a53f5370ab70873882debc61e27576f28371f
Aryan <arysin163@gmail.com>
2023-02-25 18:19:37 -0500

    Task Database and schema created - mongoose schema for database done for tasks collection

--- connector.js
+++ connector.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/dogsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todo", {useNewUrlParser: true});
 
-const dogSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  breed: String
- });
+// Defining task schema
+const taskSchema = new mongoose.Schema({
+    taskName: String,
+    taskDescription: String,
+    startDate : Date,
+    endDate : Date,
+    state : { type: Boolean, default: false },
+    updated : { type: Date, default: Date.now }     //metadata
+});
 
- const Dog = mongoose.model("Dog", dogSchema);
- const dog = new Dog({
-  name: "Rex",
-  age: 1,
-  breed: "Golden Retriever"
- });
- dog.save();
+// Creating collection
+const tasks = mongoose.model("task", taskSchema);
+
+const task1 = new tasks({
+    taskName: "Do grocery",
+    taskDescription: "Buy milk, bread, eggs, and fruits",
+    startDate : new Date("2023-05-25T12:34"),
+    endDate : new Date("2023-05-28T12:34"),
+});
+
+task1.save();
+
+// give console output of done
+console.log("saved to database");
