d11cc6d7f4ae43c81f29d766cf9230ef71ec6181
Aryan <arysin163@gmail.com>
2023-02-25 19:37:03 -0500

    function to insert documents

--- connector.js
+++ connector.js
@@ -25,3 +25,24 @@ task1.save();
 
 // give console output of done
 console.log("saved to database");
+
+// function to create new task
+function createTask(taskName, taskDescription, startDate, endDate) {
+    const task = new tasks({
+        taskName: taskName,
+        taskDescription: taskDescription,
+        startDate : startDate,
+        endDate : endDate,
+    });
+    task.save();
+    console.log("saved to database");
+}
+
+// call createTask function
+createTask("Get milk ", "Buy organic", new Date("2023-05-25T12:34"), new Date("2023-05-29T12:34"));
+
+
+// function to update task
+// findOneAndUpdate()
+//function updateTask(taskName, taskDescription, startDate, endDate) {
+
